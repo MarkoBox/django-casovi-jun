2e07f76e6d0e7c481d4cdd74bffb50a775c3e00e
Marko <Markoboki@hotmail.com>
2018-07-14 23:12:15 +0200

    Model veze i polja

--- ljudi/models.py
+++ ljudi/models.py
@@ -40,7 +40,6 @@ class Projekat(models.Model):
     naziv = models.CharField(max_length=255)
     zaposleni = models.ManyToManyField('Zaposleni', through='Projekat_Zaposleni')
 
-
     def __str__(self):
         return self.naziv
 
@@ -55,12 +54,62 @@ class Projekat_Zaposleni(models.Model):
     od_datuma = models.DateField()
     do_datuma = models.DateField()
 
+    def __str__(self):
+        return f"Zaposleni {self.zaposleni} je na projektu {self.projekat} od {self.od_datuma} do {self.do_datuma}"
+
 
 class Sredstvo_za_rad(models.Model):
     naziv = models.CharField(max_length=255)
+    vrednost = models.DecimalField(max_digits=30, decimal_places=2)
+    datum_kupovine = models.DateField()
+    zaposleni = models.ForeignKey('Zaposleni', related_name='zaposleni')
+    deo_sredstva = models.ManyToManyField('self', through="Deo_sredstva", symmetrical=False, related_name='je_deo')
+
+    def __str__(self):
+        return self.naziv
+
+
+class Deo_sredstva(models.Model):
+    iz_sredstva = models.ForeignKey(Sredstvo_za_rad, related_name='iz_sredstva')
+    u_sredstvo = models.ForeignKey(Sredstvo_za_rad, related_name='u_sredstvo')
+    procenat_promene = models.DecimalField(max_digits=1, decimal_places=2)  # ovo bi trebalo da je procenat
+    vrednost = models.DecimalField(max_digits=30, decimal_places=2)
+    datum_promene = models.DateField()
+
+    def __str__(self):
+        return self.datum_promene
+
+
+class Importovan_dokument(models.Model):
+    naziv = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    ekstenzija = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.naziv
+
+
+class Tip_odsustva(models.Model):
+    naziv = models.CharField(max_length=255)
+    Opis = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.naziv
 
 
 class Dani_odsustva(models.Model):
     Od_dana = models.DateField()
     Do_data = models.DateField()
     # Broj_dana = models.IntegerField()   kako ovo da bude kalkulativna kolona
+    dokument = models.ForeignKey('Importovan_dokument', related_name='dokument')
+    zaposleni = models.ForeignKey('Zaposleni', related_name='zaposleni')
+    tip = models.ForeignKey('Tip_odsustva', related_name='tip_odsustva')
+
+
+class Template_resenja(models.Model):
+    naziv = models.CharField(max_length=255)
+    path = models.CharField(max_length=255)
+    tip = models.ForeignKey('Tip_odsustva', related_name='tip_odsustva')
+
+    def __str__(self):
+        return self.naziv
